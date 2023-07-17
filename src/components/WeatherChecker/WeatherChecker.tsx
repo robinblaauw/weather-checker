b4b1772e1789dc525f0f6a62405e756a1303c492
robinblaauw <robinblaauw1991@gmail.com>
2023-07-17 16:22:57 +0200

    cities initial api setup

--- src/components/WeatherChecker/WeatherChecker.tsx
+++ src/components/WeatherChecker/WeatherChecker.tsx
@@ -1,11 +1,20 @@
-import React from "react";
-import "./WeatherChecker.scss";
+import React, { useState } from "react";
+import "./WeatherChecker.css";
+import { Cities } from "../CitiesDropdown/CitiesDropdown";
 
 export const WeatherChecker = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <div className="weather-checker">
-      WeatherChecker
-      <button className="weather-checker--button">Test</button>
+      <p>Weather checker app</p>
+      <button
+        className="weather-checker--button"
+        onClick={() => setIsOpen((openstate) => !openstate)}
+      >
+        {isOpen ? "close" : "open"}
+      </button>
+      {isOpen && <div>I am open</div>}
+      <Cities />
     </div>
   );
 };
