795def5289b1afd58958c5661576b527b8daadef
robinblaauw <robinblaauw1991@gmail.com>
2023-07-17 14:28:57 +0200

    initial

---
+++ src/components/WeatherChecker/WeatherChecker.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./WeatherChecker.css";
+
+export const WeatherChecker = () => {
+  return <div className="weather-checker">WeatherChecker</div>;
+};
---
+++ src/components/index.ts
@@ -0,0 +1,1 @@
+export { default as WeatherChecker } from "./WeatherChecker";
